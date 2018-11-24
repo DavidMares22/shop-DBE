3af1c7d0a13e8049b24f0c410a2bea9b9eaa67ee
David Mares <damr18@gmail.com>
2018-11-24 00:35:46 -0600

    updating product quantities and display cart status in bar

--- cart/views.py
+++ cart/views.py
@@ -25,6 +25,10 @@ def cart_remove(request, product_id):
 
 def cart_detail(request):
   cart = Cart(request)
+  for item in cart:
+    # update_quantity_form is the name of the variable in the template
+    item['update_quantity_form'] = CartAddProductForm(initial={'quantity':item['quantity'],'update':True})
+
   return render(request,'cart/detail.html', {'cart':cart})
 
   
